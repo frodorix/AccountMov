c335fb7d91e2f8b148939f40940b66b19f0b647d
Edwin Rojas <edwin.frederick@gmail.com>
2023-03-06 23:40:49 -0400

    herramietas de integracion inicializadas

--- Tools/BUILDRunSQLServer.ps1
+++ Tools/BUILDRunSQLServer.ps1
@@ -1,12 +1,12 @@
-$source = "src/db"
-$destino = "Tools/ServerMSSQL"
+#$source = "src/db"
+#$destino = "Tools/ServerMSSQL"
 
-Copy-Item -Path $source -Filter "*.sql" -Recurse -Destination $destino -Container -force
+#Copy-Item -Path $source -Filter "*.sql" -Recurse -Destination $destino -Container -force
 
 #######elimiar imagen anterior
 docker rmi --force $(docker images 'server-mssql' -q)
 
-docker build -t server-mssql Tools\MSSql\.
+docker build -t server-mssql MSSql\.
 
 #iniciar container
-docker run -d -p 8433:1433 server-mssql
+docker run -d -p 1433:1433 server-mssql
---
+++ Tools/MSSql/createdb.sql
@@ -0,0 +1,70 @@
+create database [AccountMovs]
+go
+USE [AccountMovs]
+GO
+/****** Object:  Table [dbo].[Cliente]    Script Date: 6/3/2023 23:14:12 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Cliente](
+	[ClienteId] [int] IDENTITY(1,1) NOT NULL,
+	[Nombre] [varchar](50) NOT NULL,
+	[Genero] [smallint] NOT NULL,
+	[Edad] [smallint] NOT NULL,
+	[Identificacion] [varchar](15) NOT NULL,
+	[Direccion] [varchar](50) NOT NULL,
+	[Telefono] [varchar](20) NOT NULL,
+	[Contrasena] [varchar](50) NOT NULL,
+	[Estado] [int] NOT NULL,
+ CONSTRAINT [PK_Cliente] PRIMARY KEY CLUSTERED 
+(
+	[ClienteId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Cuenta]    Script Date: 6/3/2023 23:14:12 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Cuenta](
+	[numeroCuenta] [int] IDENTITY(1,1) NOT NULL,
+	[tipo] [int] NOT NULL,
+	[saldoInicial] [decimal](18, 2) NOT NULL,
+	[clienteId] [int] NULL,
+	[Estado] [int] NOT NULL,
+ CONSTRAINT [PK_Cuenta] PRIMARY KEY CLUSTERED 
+(
+	[numeroCuenta] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Movimientos]    Script Date: 6/3/2023 23:14:12 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Movimientos](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[Fecha] [datetime] NOT NULL,
+	[tipo] [int] NOT NULL,
+	[valor] [decimal](18, 2) NOT NULL,
+	[saldo] [decimal](18, 2) NOT NULL,
+	[numeroCuenta] [int] NULL,
+ CONSTRAINT [PK_Movimientos] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[Cuenta]  WITH CHECK ADD  CONSTRAINT [FK_Cuenta_Cliente] FOREIGN KEY([clienteId])
+REFERENCES [dbo].[Cliente] ([ClienteId])
+GO
+ALTER TABLE [dbo].[Cuenta] CHECK CONSTRAINT [FK_Cuenta_Cliente]
+GO
+ALTER TABLE [dbo].[Movimientos]  WITH CHECK ADD  CONSTRAINT [FK_Movimientos_Cuenta] FOREIGN KEY([numeroCuenta])
+REFERENCES [dbo].[Cuenta] ([numeroCuenta])
+GO
+ALTER TABLE [dbo].[Movimientos] CHECK CONSTRAINT [FK_Movimientos_Cuenta]
+GO
